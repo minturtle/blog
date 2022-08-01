057f7465632a8bd90e18fce086e3bfac2a39a8d7
minturtle <minseok0425@kakao.com>
2022-08-01 15:28:26 +0900

    boardController 단위 테스트 작성완료

--- src/test/java/com/blog/blog/controller/BoardControllerTest.java
+++ src/test/java/com/blog/blog/controller/BoardControllerTest.java
@@ -1,7 +1,9 @@
 package com.blog.blog.controller;
 
+import com.blog.blog.domain.Board;
 import com.blog.blog.domain.User;
 import com.blog.blog.dto.BoardDto;
+import com.blog.blog.dto.ReplyDto;
 import com.blog.blog.service.BoardService;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import static org.mockito.BDDMockito.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,15 +23,13 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.test.web.servlet.ResultActions;
 
-import javax.servlet.http.HttpSession;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@WebMvcTest(BoardController.class) // Controller, ControllerAdvice, Filter이 등록됨.
+// Controller, ControllerAdvice, Filter이 등록됨.
+@WebMvcTest(value = BoardController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 @AutoConfigureMockMvc //MockMVC를 IoC에 등록해줌.
 public class BoardControllerTest {
 
@@ -37,8 +38,6 @@ public class BoardControllerTest {
 
     @MockBean //IoC환경에서 실제 bean대신 들어감.
     private BoardService boardService;
-
-
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -54,14 +53,71 @@ public class BoardControllerTest {
         User user = new User("root", "a2@naver.com", "1111");
         httpSession.setAttribute("user", user);
 
-
         //when
         ResultActions action = mockMvc.perform(post("/board/create")
                 .session(httpSession)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(dtoJSON).accept(MediaType.ALL));
+
         //then
-        //403 에러 발생, 왜일까?
-        action.andExpect(status().is3xxRedirection()).andExpect(content().string("redirect:/"));
+        action.andExpect(status().is3xxRedirection()).andExpect(redirectedUrl("/"));
+    }
+
+
+    @Test
+    @DisplayName("boardForm GET 테스트")
+    void t2() throws Exception{
+        //given
+        //when
+        ResultActions res = mockMvc.perform(get("/board/create"));
+        //then
+        res.andExpect(view().name("board/boardForm"));
+    }
+
+    @Test
+    @DisplayName("board id로 조회하기 테스트")
+    void t3() throws Exception {
+        //given
+        Board board = new Board("title", "test content", new User("root", "2@nav.com", "pw"));
+        int boardId = 1;
+        board.setId(1);
+        given(boardService.getBoardById(boardId)).willReturn(board);
+        //when
+        ResultActions res = mockMvc.perform(get("/board/" + boardId));
+        //then
+        res.andExpect(model().attribute("board", board)).andExpect(view().name("board/board"));
+    }
+
+    @Test
+    @DisplayName("없는 board 조회하기 테스트")
+    void t4() throws Exception {
+        //given
+        int boardId = 1;
+        given(boardService.getBoardById(boardId)).willThrow(IllegalArgumentException.class);
+        //when
+        ResultActions res = mockMvc.perform(get("/board/" + boardId));
+        //then
+        res.andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    @DisplayName("댓글 달기 테스트")
+    void t5() throws Exception {
+        //given
+        MockHttpSession httpSession = new MockHttpSession();
+        int boardId = 1;
+        User user = new User("root", "a2@naver.com", "1111");
+        Board board = new Board("title", "con", user);
+        ReplyDto replyDto = new ReplyDto("Hello reply", boardId);
+        String replyDtoJson = objectMapper.writeValueAsString(replyDto);
+
+        board.setId(1);
+        httpSession.setAttribute("user", user);
+        //when
+        ResultActions res = mockMvc.perform(post("/board/reply")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(replyDtoJson).session(httpSession).accept(MediaType.ALL));
+        //then
+        res.andExpect(status().isOk());
     }
 }
